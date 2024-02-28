c648f58c398dc63a4f43147e16edc6d5d5831579
sean-cycode <160075144+sean-cycode@users.noreply.github.com>
2024-02-28 08:30:01 -0500

    Create sample.tf

---
+++ sample.tf
@@ -0,0 +1,24 @@
+output "plan_key_id" {
+    value = ""
+    description = "The Id of a usage plan key."
+}
+
+output "key_id" {
+    value = "${aws_api_gateway_usage_plan_key.usage_plan_key.key_id}"
+    description = "The identifier of the API gateway key."
+}
+
+output "usage_plan_id" {
+    value = "${aws_api_gateway_usage_plan_key.usage_plan_key.usage_plan_id}"
+    description = "The identifier of the usage plan."
+}
+
+output "usage_plan_key_name" {
+    value = "${aws_api_gateway_usage_plan_key.usage_plan_key.name}"
+    description = "The name of a usage plan key."
+}
+
+output "api_key_value" {
+    value = "${aws_api_gateway_api_key.api_key.value}"
+    description = "The value of the API key."
+}
